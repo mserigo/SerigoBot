f584161824b3423779829b4ce07ec88c066c9658
mserigo <mserigo@gmail.com>
2016-09-15 18:52:50 -0300

    new pass

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: 'YourAppId', appPassword: 'NN74S76qrmWbjmUeHKWQzWL' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
